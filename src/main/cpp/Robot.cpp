871d45352482f2c1b545b8d84513499be3b73ee5
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-05-24 18:36:39 -0500

    5/24/21

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -57,7 +57,7 @@ void Robot::AutonomousPeriodic() {
         m_hopper.Set(0.0);
         continue;
     }
-    while(m_timer.Get() > 11 && m_timer.Get() < 12)
+    while(m_timer.Get() > 11 && m_timer.Get() < 14)
     {
         m_rightBottomMotor.Set(.3);
         m_rightMiddleMotor.Set(.3);
@@ -66,6 +66,55 @@ void Robot::AutonomousPeriodic() {
         m_leftMiddleMotor.Set(-.3);
         continue;
     }
+    while(m_timer.Get() > 14 && m_timer.Get() < 15)
+    {
+        m_rightBottomMotor.Set(0);
+        m_rightMiddleMotor.Set(0);
+
+        m_leftBottomMotor.Set(0);
+        m_leftMiddleMotor.Set(0);
+        continue;
+    }
+    while(m_timer.Get() > 15 && m_timer.Get() < 16.5)
+    {
+        m_rightBottomMotor.Set(-.3);
+        m_rightMiddleMotor.Set(-.3);
+
+        m_leftBottomMotor.Set(.3);
+        m_leftMiddleMotor.Set(.3);
+        continue;
+    }
+    while(m_timer.Get() > 16.5 && m_timer.Get() < 17.5)
+    {
+        m_rightBottomMotor.Set(0);
+        m_rightMiddleMotor.Set(0);
+
+        m_leftBottomMotor.Set(0);
+        m_leftMiddleMotor.Set(0);
+        continue;
+    }
+    while(m_timer.Get() > 17.5 && m_timer.Get() < 20.5 ){
+        //shooter.AutoPID();
+        m_leftShooter.Set(.6); 
+        m_rightShooter.Set(.6); 
+        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
+        //m_hopper.Set(.3);
+        continue;
+    }
+    while(m_timer.Get() > 20.5 && m_timer.Get() < 27.5 ){
+        m_hopper.Set(.3);
+        m_leftShooter.Set(.6); 
+        m_rightShooter.Set(.6); 
+        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
+    }
+    while(m_timer.Get() > 27.5 && m_timer.Get() < 28.5){ 
+        shooterHood.Set(frc::DoubleSolenoid::Value::kReverse);
+        m_leftShooter.Set(0);
+        m_rightShooter.Set(0);
+        m_hopper.Set(0.0);
+        continue;
+    }
+    
     m_rightBottomMotor.Set(0);
     m_rightMiddleMotor.Set(0);
 
